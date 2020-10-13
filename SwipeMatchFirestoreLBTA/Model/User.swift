22b5b981aa49df2dbe0629d541e4e863ead1b981
coleriggle <coleriggle@gmail.com>
2020-10-13 18:33:17 -0400

    Added support for multiple user images

--- SwipeMatchFirestoreLBTA/Model/User.swift
+++ SwipeMatchFirestoreLBTA/Model/User.swift
@@ -13,8 +13,9 @@ struct User: ProducesCardViewModel {
     var name: String?
     var age: Int?
     var profession: String?
-    //let imageNames: [String]
     var imageUrl1: String?
+    var imageUrl2: String?
+    var imageUrl3: String?
     var uid: String?
     
     init(dictionary: [String: Any]) {
@@ -22,6 +23,8 @@ struct User: ProducesCardViewModel {
         self.profession = dictionary["profession"] as? String
         self.name = dictionary["fullName"] as? String
         self.imageUrl1 = dictionary["imageUrl1"] as? String
+        self.imageUrl2 = dictionary["imageUrl2"] as? String
+        self.imageUrl3 = dictionary["imageUrl3"] as? String
         self.uid = dictionary["uid"] as? String
     }
     
@@ -34,7 +37,18 @@ struct User: ProducesCardViewModel {
         attributedText.append(NSAttributedString(string: "  \(ageString)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
         attributedText.append(NSAttributedString(string: "\n\(profession ?? "")", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
         
-        return CardViewModel(imageNames: [imageUrl1 ?? ""], attributedString: attributedText, textAlignment: .left)
+        var imageUrls = [String]()
+        if let url = imageUrl1 {
+            imageUrls.append(url);
+        }
+        if let url = imageUrl2 {
+            imageUrls.append(url);
+        }
+        if let url = imageUrl3 {
+            imageUrls.append(url);
+        }
+        
+        return CardViewModel(imageNames: imageUrls, attributedString: attributedText, textAlignment: .left)
         
     }
 }
