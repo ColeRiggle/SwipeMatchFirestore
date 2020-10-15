968a422e60a4619ca7b33bef5f7f943e8de63d0a
coleriggle <coleriggle@gmail.com>
2020-10-15 13:57:16 -0400

    Video #29

--- SwipeMatchFirestoreLBTA/Controllers/SettingsController.swift
+++ SwipeMatchFirestoreLBTA/Controllers/SettingsController.swift
@@ -17,7 +17,7 @@ class CustomImagePickerController: UIImagePickerController {
 }
 
 class SettingsController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+
     // instance properties
     lazy var image1Button = createButton(selector: #selector(handleSelectPhoto))
     lazy var image2Button = createButton(selector: #selector(handleSelectPhoto))
@@ -33,33 +33,36 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let selectedImage = info[.originalImage] as? UIImage
-        
+        // how do i set the image on my buttons when I select a photo?
         let imageButton = (picker as? CustomImagePickerController)?.imageButton
         imageButton?.setImage(selectedImage?.withRenderingMode(.alwaysOriginal), for: .normal)
         dismiss(animated: true)
         
-        let hud = JGProgressHUD(style: .dark)
-        hud.textLabel.text = "Uploading image..."
-        hud.show(in: view)
         let filename = UUID().uuidString
         let ref = Storage.storage().reference(withPath: "/images/\(filename)")
         guard let uploadData = selectedImage?.jpegData(compressionQuality: 0.75) else { return }
-        ref.putData(uploadData, metadata: nil) { (nil, error) in
-            if let error = error {
+        
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Uploading image..."
+        hud.show(in: view)
+        ref.putData(uploadData, metadata: nil) { (nil, err) in
+            if let err = err {
                 hud.dismiss()
-                print("Failed to upload image to stoage: ", error)
+                print("Failed to upload image to storage:", err)
                 return
             }
             
-            ref.downloadURL { (url, error) in
+            print("Finished uploading image")
+            ref.downloadURL(completion: { (url, err) in
+                
                 hud.dismiss()
                 
-                if let error = error {
-                    print("Failed to retrieve download URL:: ", error)
+                if let err = err {
+                    print("Failed to retrieve download URL:", err)
                     return
                 }
                 
-                print("Finished getting download url:", url?.absoluteURL ?? "")
+                print("Finished getting download url:", url?.absoluteString ?? "")
                 
                 if imageButton == self.image1Button {
                     self.user?.imageUrl1 = url?.absoluteString
@@ -68,9 +71,7 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
                 } else {
                     self.user?.imageUrl3 = url?.absoluteString
                 }
-                
-                
-            }
+            })
         }
     }
     
@@ -122,13 +123,11 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
                 self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
             }
         }
-        
         if let imageUrl = user?.imageUrl2, let url = URL(string: imageUrl) {
             SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
                 self.image2Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
             }
         }
-        
         if let imageUrl = user?.imageUrl3, let url = URL(string: imageUrl) {
             SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
                 self.image3Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
@@ -160,6 +159,9 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        //tableView.allowsSelection = false
+        tableView.isUserInteractionEnabled = true
+        
         if section == 0 {
             return header
         }
@@ -171,9 +173,12 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
             headerLabel.text = "Profession"
         case 3:
             headerLabel.text = "Age"
-        default:
+        case 4:
             headerLabel.text = "Bio"
+        default:
+            headerLabel.text = "Seeking Age Range"
         }
+        headerLabel.font = UIFont.boldSystemFont(ofSize: 16)
         return headerLabel
     }
     
@@ -185,14 +190,48 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 5
+        return 6
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return section == 0 ? 0 : 1
     }
     
+    @objc fileprivate func handleMinAgeChange(slider: UISlider) {
+        evaluateMinMax()
+    }
+    
+    @objc fileprivate func handleMaxAgeChange(slider: UISlider) {
+        evaluateMinMax()
+    }
+    
+    fileprivate func evaluateMinMax() {
+        guard let ageRangeCell = tableView.cellForRow(at: [5, 0]) as? AgeRangeCell else { return }
+        let minValue = Int(ageRangeCell.minSlider.value)
+        var maxValue = Int(ageRangeCell.maxSlider.value)
+        maxValue = max(minValue, maxValue)
+        ageRangeCell.maxSlider.value = Float(maxValue)
+        ageRangeCell.minLabel.text = "Min \(minValue)"
+        ageRangeCell.maxLabel.text = "Max \(maxValue)"
+        
+        user?.minSeekingAge = minValue
+        user?.maxSeekingAge = maxValue
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // age range cell
+        if indexPath.section == 5 {
+            let ageRangeCell = AgeRangeCell(style: .default, reuseIdentifier: nil)
+            ageRangeCell.minSlider.addTarget(self, action: #selector(handleMinAgeChange), for: .valueChanged)
+            ageRangeCell.maxSlider.addTarget(self, action: #selector(handleMaxAgeChange), for: .valueChanged)
+            // we need to set up the labels on our cell here
+            ageRangeCell.minLabel.text = "Min \(user?.minSeekingAge ?? -1)"
+            ageRangeCell.maxLabel.text = "Max \(user?.maxSeekingAge ?? -1)"
+            ageRangeCell.minSlider.value = Float(user?.minSeekingAge ?? -1)
+            ageRangeCell.maxSlider.value = Float(user?.maxSeekingAge ?? -1)
+            return ageRangeCell
+        }
+        
         let cell = SettingsCell(style: .default, reuseIdentifier: nil)
         
         switch indexPath.section {
@@ -206,10 +245,10 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
             cell.textField.addTarget(self, action: #selector(handleProfessionChange), for: .editingChanged)
         case 3:
             cell.textField.placeholder = "Enter Age"
+            cell.textField.addTarget(self, action: #selector(handleAgeChange), for: .editingChanged)
             if let age = user?.age {
                 cell.textField.text = String(age)
             }
-            cell.textField.addTarget(self, action: #selector(handleAgeChange), for: .editingChanged)
         default:
             cell.textField.placeholder = "Enter Bio"
         }
@@ -217,15 +256,15 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         return cell
     }
     
-    @objc fileprivate func handleNameChange(textField: UITextView) {
+    @objc fileprivate func handleNameChange(textField: UITextField) {
         self.user?.name = textField.text
     }
     
-    @objc fileprivate func handleProfessionChange(textField: UITextView) {
+    @objc fileprivate func handleProfessionChange(textField: UITextField) {
         self.user?.profession = textField.text
     }
     
-    @objc fileprivate func handleAgeChange(textField: UITextView) {
+    @objc fileprivate func handleAgeChange(textField: UITextField) {
         self.user?.age = Int(textField.text ?? "")
     }
     
@@ -240,6 +279,7 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     }
     
     @objc fileprivate func handleSave() {
+        print("Saving our settings data into Firestore")
         guard let uid = Auth.auth().currentUser?.uid else { return }
         let docData: [String: Any] = [
             "uid": uid,
@@ -248,19 +288,22 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
             "imageUrl2": user?.imageUrl2 ?? "",
             "imageUrl3": user?.imageUrl3 ?? "",
             "age": user?.age ?? -1,
-            "profession": user?.profession ?? ""
+            "profession": user?.profession ?? "",
+            "minSeekingAge": user?.minSeekingAge ?? -1,
+            "maxSeekingAge": user?.maxSeekingAge ?? -1
         ]
+        
         let hud = JGProgressHUD(style: .dark)
         hud.textLabel.text = "Saving settings"
         hud.show(in: view)
-        Firestore.firestore().collection("users").document(uid).setData(docData) { (error) in
+        Firestore.firestore().collection("users").document(uid).setData(docData) { (err) in
             hud.dismiss()
-            if let error = error {
-                print(error)
+            if let err = err {
+                print("Failed to save user settings:", err)
                 return
             }
             
-            print("Successfully saved user settings.")
+            print("Finished saving user info")
         }
     }
     
@@ -269,3 +312,4 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
     }
 
 }
+
