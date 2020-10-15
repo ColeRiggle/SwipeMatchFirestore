c4b4a295ebd834945ae1305f64d1879aad10d20f
coleriggle <coleriggle@gmail.com>
2020-10-15 13:57:56 -0400

    Added SettingsController

---
+++ SwipeMatchFirestoreLBTA/Views/SettingsCell.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsCell.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by Cole Riggle on 10/10/20.
+//  Copyright © 2020 Cole Riggle. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+    
+    class SettingsTextField: UITextField {
+        
+        override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 0, height: 44)
+        }
+    }
+    
+    let textField: UITextField = {
+        let tf = SettingsTextField()
+        tf.placeholder = "Enter Name"
+        return tf
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(textField)
+        textField.fillSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+}
